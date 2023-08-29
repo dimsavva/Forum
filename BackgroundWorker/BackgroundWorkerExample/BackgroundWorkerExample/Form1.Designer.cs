89d7c7c6d944b84be1992806a0463dea64c7083a
Sesethu Mhlana <smhlana98@gmail.com>
2023-08-29 21:36:09 +0200

    BackgroundWorker example.

---
+++ BackgroundWorker/BackgroundWorkerExample/BackgroundWorkerExample/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace BackgroundWorkerExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            processButton = new Button();
+            cancelButton = new Button();
+            progressBar = new ProgressBar();
+            progressLabel = new Label();
+            backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            randomNumberButton = new Button();
+            randomNumberLabel = new Label();
+            SuspendLayout();
+            // 
+            // processButton
+            // 
+            processButton.Location = new Point(12, 12);
+            processButton.Name = "processButton";
+            processButton.Size = new Size(138, 29);
+            processButton.TabIndex = 0;
+            processButton.Text = "Process";
+            processButton.UseVisualStyleBackColor = true;
+            processButton.Click += processButton_Click;
+            // 
+            // cancelButton
+            // 
+            cancelButton.Location = new Point(174, 12);
+            cancelButton.Name = "cancelButton";
+            cancelButton.Size = new Size(139, 29);
+            cancelButton.TabIndex = 1;
+            cancelButton.Text = "Cancel";
+            cancelButton.UseVisualStyleBackColor = true;
+            cancelButton.Click += cancelButton_Click;
+            // 
+            // progressBar
+            // 
+            progressBar.Location = new Point(12, 53);
+            progressBar.Name = "progressBar";
+            progressBar.Size = new Size(301, 29);
+            progressBar.TabIndex = 2;
+            // 
+            // progressLabel
+            // 
+            progressLabel.AutoSize = true;
+            progressLabel.Location = new Point(12, 96);
+            progressLabel.Name = "progressLabel";
+            progressLabel.Size = new Size(29, 20);
+            progressLabel.TabIndex = 3;
+            progressLabel.Text = "0%";
+            // 
+            // backgroundWorker
+            // 
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.WorkerSupportsCancellation = true;
+            backgroundWorker.DoWork += backgroundWorker_DoWork;
+            backgroundWorker.ProgressChanged += backgroundWorker_ProgressChanged;
+            backgroundWorker.RunWorkerCompleted += backgroundWorker_RunWorkerCompleted;
+            // 
+            // randomNumberButton
+            // 
+            randomNumberButton.Location = new Point(12, 138);
+            randomNumberButton.Name = "randomNumberButton";
+            randomNumberButton.Size = new Size(138, 29);
+            randomNumberButton.TabIndex = 4;
+            randomNumberButton.Text = "Random Number";
+            randomNumberButton.UseVisualStyleBackColor = true;
+            randomNumberButton.Click += button1_Click;
+            // 
+            // randomNumberLabel
+            // 
+            randomNumberLabel.AutoSize = true;
+            randomNumberLabel.Location = new Point(203, 142);
+            randomNumberLabel.Name = "randomNumberLabel";
+            randomNumberLabel.Size = new Size(17, 20);
+            randomNumberLabel.TabIndex = 5;
+            randomNumberLabel.Text = "0";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(330, 187);
+            Controls.Add(randomNumberLabel);
+            Controls.Add(randomNumberButton);
+            Controls.Add(progressLabel);
+            Controls.Add(progressBar);
+            Controls.Add(cancelButton);
+            Controls.Add(processButton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button processButton;
+        private Button cancelButton;
+        private ProgressBar progressBar;
+        private Label progressLabel;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private Button randomNumberButton;
+        private Label randomNumberLabel;
+    }
+}
---
+++ BackgroundWorker/BackgroundWorkerExample/BackgroundWorkerExample/Form1.cs
@@ -0,0 +1,84 @@
+namespace BackgroundWorkerExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void backgroundWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            int sum = 0;
+            // You can use a try-catch block to handle an exception
+            //try
+            //{
+                for (int i = 0; i <= 100; i++)
+                {
+                    Thread.Sleep(100);
+                    sum += i;
+                    backgroundWorker.ReportProgress(i);
+
+                    if (backgroundWorker.CancellationPending)
+                    {
+                        e.Cancel = true;
+                        backgroundWorker.ReportProgress(0);
+                        return;
+                    }
+
+                    // If an exception is thrown it will be added to the RunWorkerCompletedEventArgs.Error
+                    // and can be accessed in the RunWorkerCompleted event handler.
+                    // Uncomment the next line to see what happens when a exception is thrown.
+                    // throw new InvalidOperationException("Random exception.");
+                }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.StackTrace, ex.Message);
+            //    If the exception is not thrown, RunWorkerCompletedEventArgs.Error will be null
+            //    throw;
+            //}
+
+            e.Result = sum;
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+            progressLabel.Text = e.ProgressPercentage.ToString() + "%";
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                progressLabel.Text = "Processing cancelled";
+            }
+            else if (e.Error is not null)
+            {
+                progressLabel.Text = e.Error.Message;
+            }
+        }
+
+        private void processButton_Click(object sender, EventArgs e)
+        {
+            if (!backgroundWorker.IsBusy)
+            {
+                backgroundWorker.RunWorkerAsync();
+            }
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker.IsBusy)
+            {
+                backgroundWorker.CancelAsync();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            randomNumberLabel.Text = new Random().Next(0, 100).ToString();
+        }
+    }
+}
