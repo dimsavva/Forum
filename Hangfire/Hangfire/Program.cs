7ea79ce7d221d3dc0eb27999072a9f2f3a91611f
Dimitrios Savva <dimsavva@gmail.com>
2023-08-31 09:50:53 +0200

    Add Hangfire Example

---
+++ Hangfire/Hangfire/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Hangfire;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Hangfire.MemoryStorage;
+using Microsoft.Extensions.Logging;
+
+namespace HangfireExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+             .ConfigureLogging(logging =>
+             {
+                 logging.ClearProviders();
+                 logging.AddConsole();
+                 logging.AddFilter(new Func<LogLevel, bool>(level => level == LogLevel.None));
+                 })
+             .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHangfire(configuration => configuration
+                        .UseSimpleAssemblyNameTypeSerializer()
+                        .UseRecommendedSerializerSettings()
+                        .UseMemoryStorage());
+
+                    services.AddHangfireServer();
+
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.Configure(app =>
+                    {
+                        app.UseHangfireDashboard();
+                        // Now it's safe to use the Hangfire API to schedule jobs
+                        RecurringJob.AddOrUpdate("fetch-crypto-prices", () => FetchAndDisplayCryptoPrices(), "*/5 * * * * *");
+                    });
+                });
+
+        public static async Task FetchAndDisplayCryptoPrices()
+        {
+            try
+            {
+                Console.WriteLine("Fetching crypto prices...");
+
+                HttpClient httpClient = new HttpClient();
+                HttpResponseMessage response = await httpClient.GetAsync("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,litecoin,ethereum,ripple,cardano&vs_currencies=zar");
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                JObject jsonResult = JObject.Parse(responseBody);
+
+                Console.WriteLine($"Bitcoin: R {string.Format("{0:n0}", jsonResult["bitcoin"]["zar"])}");
+                Console.WriteLine($"Litecoin: R {string.Format("{0:n0}", jsonResult["litecoin"]["zar"])}");
+                Console.WriteLine($"Ethereum: R {string.Format("{0:n0}", jsonResult["ethereum"]["zar"])}");
+                Console.WriteLine($"Ripple: R {string.Format("{0:n0}", jsonResult["ripple"]["zar"])}");
+                Console.WriteLine($"Cardano: R {string.Format("{0:n0}", jsonResult["cardano"]["zar"])}");
+
+                Console.WriteLine("Job completed successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
